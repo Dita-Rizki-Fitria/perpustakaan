cf7154e9e9dd57b93c28c9b15b1b165658bb2571
tata-01 <ditaf2512@gmail.com>
2023-11-13 13:11:45 +0700

    pidahan

--- asset/js/lock.js
+++ asset/js/lock.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // alert(window.location.origin+'/perpustakaan/tamplate/header.html');
-    $("#top").load(window.location.origin+'/perpustakaan/tamplate/header.html'); 
+    $("#top").load('tamplate/header.html'); 
     // $("#top").text("test");
 
   });
