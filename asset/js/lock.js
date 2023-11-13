2f59e2e00bf2322ddcc13ac1067d75363c9b0f21
annisa-damayanti <131932987+annisa-damayanti@users.noreply.github.com>
2023-11-13 16:47:35 +0700

    perubahan template

--- asset/js/lock.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    // alert(window.location.origin+'/perpustakaan/tamplate/header.html');
-    $("#top").load('tamplate/header.html'); 
-    // $("#top").text("test");
-
-  });
---
+++ asset/js/lock_navbar.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // alert(window.location.origin + '/perpustakaan/tamplate/header.html');
+    $("#kiri").load('tamplate/navbar.html');
+    // $("#top").text("test");
+
+});
